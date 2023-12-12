e3d6c7c12d8cac8b857be8250b00b94240a3d9ad
RTxsivex <111413108+RTxsivex@users.noreply.github.com>
2023-12-12 16:58:39 +0300

    Update ⊗ppPmFmPrm.php

--- ⊗ppPmFmPrm.php
+++ ⊗ppPmFmPrm.php
@@ -199,4 +199,47 @@ echo '</pre>';
 print_r(date('D',strtotime($_GET['day'].".".preg_replace('#[а-яёА-ЯЁ]+#u','',$_GET['month']).$_GET['year'])));
 
 #11
+<form action="" method="get">
+    <input name="day"value="<?=$_GET['day']?? 1?>"><br><br>
+    <input name="month"value="<?=$_GET['month']?? 1?>"><br><br>
+    <input type="submit">
+</form>
+
+<?php
+function getZodiacSign($day, $month){
+    if (($month == 3 && $day >= 21) || ($month == 4 && $day <= 19)) {
+        return "Овен";
+    } elseif (($month == 4 && $day >= 20) || ($month == 5 && $day <= 20)) {
+        return "Телец";
+    } elseif (($month == 5 && $day >= 21) || ($month == 6 && $day <= 20)) {
+        return "Близнецы";
+    } elseif (($month == 6 && $day >= 21) || ($month == 7 && $day <= 22)) {
+        return "Рак";
+    } elseif (($month == 7 && $day >= 23) || ($month == 8 && $day <= 22)) {
+        return "Лев";
+    } elseif (($month == 8 && $day >= 23) || ($month == 9 && $day <= 22)) {
+        return "Дева";
+    } elseif (($month == 9 && $day >= 23) || ($month == 10 && $day <= 22)) {
+        return "Весы";
+    } elseif (($month == 10 && $day >= 23) || ($month == 11 && $day <= 21)) {
+        return "Скорпион";
+    } elseif (($month == 11 && $day >= 22) || ($month == 12 && $day <= 21)) {
+        return "Стрелец";
+    } elseif (($month == 12 && $day >= 22) || ($month == 1 && $day <= 19)) {
+        return "Козерог";
+    } elseif (($month == 1 && $day >= 20) || ($month == 2 && $day <= 18)) {
+        return "Водолей";
+    } elseif (($month == 2 && $day >= 19) || ($month == 3 && $day <= 20)) {
+        return "Рыбы";
+    }
+    return "Не удалось определить знак зодиака";
+}
+
+// Получаем день и месяц из GET-параметров
+$day = isset($_GET['day']) ? intval($_GET['day']) : 1;
+$month = isset($_GET['month']) ? intval($_GET['month']) : 1;
+
+// Определяем знак зодиака
+$zodiac = getZodiacSign($day, $month);
+echo "Ваш знак зодиака: " . $zodiac;
 
